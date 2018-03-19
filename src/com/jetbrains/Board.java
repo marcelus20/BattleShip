6c72fc7c89adaa115cb801c82be64c5cc2b48b79
Felipe <marcelus20felipe@gmail.com>
2018-03-19 00:02:12 +0000

    created the toString method of board

--- src/com/jetbrains/Board.java
+++ src/com/jetbrains/Board.java
@@ -5,7 +5,7 @@ public class Board {
 
     public final Integer row;
     public final Integer cols;
-    public final Boolean [][] fetchContent;
+    public final BoardStates [][] fetchContent;
 
 
 
@@ -15,14 +15,62 @@ public class Board {
         this.fetchContent = initFetchContent();
     }
 
-    private Boolean[][] initFetchContent(){
-        final Boolean[][] tempFetch = new Boolean[this.row][this.cols];
+    /**
+     * THIS IS A PRIVATE CONSTRUCTOR, THIS WILL BE ACTIVATED WHEN USED THE FUNCTION FETCHCOORDENATE
+     * THAT RETURNS A NEW BOARD OBJECT DUE TO THE IMMUTABILITY.
+     * @param fetchContent
+     */
+    private Board(BoardStates[][] fetchContent){
+        this.fetchContent = fetchContent;
+        this.row = this.fetchContent.length;
+        this.cols = this.fetchContent[0].length;
+    }
+
+    private BoardStates[][] initFetchContent(){
+        final BoardStates[][] tempFetch = new BoardStates[this.row][this.cols];
         for(int i = 0; i < this.row; i++){
             for(int j = 0; j < this.cols; j++){
-                tempFetch[i][j] = false;
+                tempFetch[i][j] = BoardStates.notRevealed;
             }
         }
 
         return tempFetch;
+    }
+
+    public Board fetchesCoordenate(final Integer row, final Integer cols){
+        BoardStates[][] content = new BoardStates[this.row][this.cols];
+        for(int i = 0; i < this.row; i++){
+            for(int j = 0; j < this.cols; j++){
+                if(i == row && j == cols){
+                    content[i][j] = BoardStates.revealedButNoShip;
+                }else{
+                    content[i][j] = BoardStates.notRevealed;
+                }
+            }
+        }
+        return new Board(content);
+    }
+
+    @Override
+    public String toString() {
+        String board = "";
+        String item = "";
+
+        for(int i = 0; i < this.row; i++){
+            board+="|";
+            for(int j = 0; j < this.row; j++){
+                if(this.fetchContent[i][j] == BoardStates.notRevealed){
+                    item = " _ |";
+                }else if(this.fetchContent[i][j] == BoardStates.revealedButNoShip){
+                    item = " * |";
+                }else{
+                    item = " H |";
+                }
+                board +=item;
+            }
+            board += "\n";
+        }
+
+        return board;
     }
 }
--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,8 +1,15 @@
 package com.jetbrains;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Board board = new Board(10,10);
+        System.out.println(Arrays.deepToString(board.fetchContent));
+        board = board.fetchesCoordenate(1,5);
+        System.out.println(Arrays.deepToString(board.fetchContent));
+        System.out.println(board);
     }
 }
