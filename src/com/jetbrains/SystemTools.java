b6fc606b450ca4389abf1464f719ba81624fab41
Felipe <marcelus20felipe@gmail.com>
2018-03-20 23:26:55 +0000

    created getInput method

--- src/com/jetbrains/SystemTools.java
+++ src/com/jetbrains/SystemTools.java
@@ -1,12 +1,25 @@
 package com.jetbrains;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SystemTools {
 
     public String getInput(String msg, String regex, String msgError){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return null;
+        String input = "";
+
+        do{
+            try{
+                input = br.readLine();
+            }catch(IOException e){
+                System.out.println(e);
+            }
+            if(!input.matches(regex)){
+                System.out.println(msgError);
+            }
+        }while(!input.matches(regex));
+        return input;
     }
 }
