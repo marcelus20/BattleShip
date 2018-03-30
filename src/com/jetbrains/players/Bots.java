4e634832b31198526fe6c4227372548ee49cda47
Felipe <marcelus20felipe@gmail.com>
2018-03-30 06:44:07 +0000

    finished Bots class

--- src/com/jetbrains/players/Bots.java
+++ src/com/jetbrains/players/Bots.java
@@ -1,4 +1,25 @@
 package com.jetbrains.players;
 
-public class Bots {
+import com.jetbrains.tools.SystemTools;
+
+public class Bots extends Player{
+
+
+    /**
+     * The bot class will have exactly the same method as the Player one, the only difference is its toString, that won't
+     * show age or email and the way it is initialised, which is needed just a name.
+     * @param name
+     */
+    public Bots(final String name) {
+        super(name, null, null);
+    }
+
+    @Override
+    public String toString() {
+        return "====Bot======" +
+                "name: " + name + "\n" +
+                "hits: " + hits + "\n"+
+                "miss: " + miss + "\n"+
+                "attempts: " + attempts;
+    }
 }
